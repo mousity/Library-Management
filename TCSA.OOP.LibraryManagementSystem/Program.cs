1b697aee648ddd39826ce502b3ea05ce67a9e2f9
Mousity <samr.mouna@gmail.com>
2025-11-15 19:39:59 -0500

    Initial commit

---
+++ TCSA.OOP.LibraryManagementSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Spectre.Console;
+
+var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
+var books = new List<string>()
+{
+    "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
+};
+
+while (true)
+{
+    Console.Clear();
+
+    var choice = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+        .Title("What do you want to do next?")
+        .AddChoices(menuChoices));
+
+    switch (choice)
+    {
+        case "View Books":
+            break;
+        case "Add Book":
+            break;
+        case "Delete Book":
+            break;
+    }
+
+}
+
